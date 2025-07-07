34b4f45f3a14f9b38a660ae906c98b8358609876
redowan <hossain2305341310@diu.edu.bd>
2025-07-07 20:31:57 +0600

    Join multiple variable in tuple

---
+++ Tuples.py/Join Tuple.py
@@ -0,0 +1,19 @@
+
+
+# Join two or tuple using (+)
+
+tuple1 = ("apple", "banana","pinapple")
+tuple2 = (1,2,3)
+
+tuple3 = tuple2 + tuple1
+print(tuple3)
+# Output: (1, 2, 3, 'apple', 'banana', 'pinapple')
+
+
+# If we want to multiply the content of a tuple a given number of times, we can use the * operator:
+
+tuple4 = ("Redowan", "Hossain")
+tuple5 = tuple4 * 3
+
+print(tuple5)
+# Output: ('Redowan', 'Hossain', 'Redowan', 'Hossain', 'Redowan', 'Hossain')
