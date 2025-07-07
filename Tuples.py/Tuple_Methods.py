7826c765ef05a091abfd7aed59110ce1924992d3
redowan <hossain2305341310@diu.edu.bd>
2025-07-07 23:07:24 +0600

    Introduction to Sets of python

--- Tuples.py/Tuple_Methods.py
+++ Tuples.py/Tuple_Methods.py
@@ -1,4 +1,4 @@
 
 # Methods
-count() # returns the number of times a specifired value occurs in a tuple
-index() # Searches the tple for a specified value and returns the position of where it was found
+# count() # returns the number of times a specifired value occurs in a tuple
+# index() # Searches the tple for a specified value and returns the position of where it was found
