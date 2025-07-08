306fba2adc86125095011a31e92d7980ca8256c8
redowan <hossain2305341310@diu.edu.bd>
2025-07-08 09:36:07 +0600

    Access iteam using for loop and x keyword

---
+++ Python Sets/Access_Set_Iteam.py
@@ -0,0 +1,28 @@
+
+# We can not access set iteam by index
+
+# Access iteam using for loop and x keyword
+
+set1 = {"Md", "Redowan", "Hossain"}
+for x in set1:
+    print(x)
+# Output: 
+# Md
+# Redowan
+# Hossain
+
+
+# Check Redowan is present in the set
+
+set2 = {"Md", "Redowan", "Hossain"}
+print("Redowan" in set2)
+
+# Output: True [if this iteam unavalable in this set output: False]
+
+
+# Check Eram are not present in the set
+
+set3 = {"Md", "Redowan", "Hossain"}
+print("Eram" in set3)
+
+# Output : False
