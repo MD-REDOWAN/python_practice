3f334fbde6fe68933484d324e3750041e37337d0
redowan <hossain2305341310@diu.edu.bd>
2025-07-08 12:29:23 +0600

    Using for loop in set

---
+++ Python Sets/Loop_Set.py
@@ -0,0 +1,16 @@
+
+
+# we can just use for loop for set
+
+set1 = {"MD", "Redowan","Hossain"}
+
+for x in set1:
+    print(x)
+
+# Output: show the random index output
+
+"""
+Hossain
+MD
+Redowan
+"""
