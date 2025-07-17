97adc48ee5dc22d981e0fea2d25ff5c4ab4addf2
MD.REDOWAN HOSSAIN <158824924+MD-REDOWAN@users.noreply.github.com>
2025-07-17 14:24:34 +0600

    About OOP concept in python

---
+++ OOP.py
@@ -0,0 +1,27 @@
+"""OOP.py
+# Advantages of OOP
+
+Provides a clear structure to programs
+Makes code easier to maintain, reuse, and debug
+Helps keep your code DRY (Don't Repeat Yourself)
+Allows you to build reusable applications with less code
+
+"""
+
+Diffrence of Class and objects
+
+Example1:
+
+Fruits = ["apple", "banana", "cherry"]
+
+class are Fruits
+object are ["apple", "banana", "cherry"]
+
+Example2 :
+
+person1 = ["name", "id", "age"]
+
+class are person1
+    
+object are ["name", "id", "age"]
+
