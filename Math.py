6021b71b947bd2a78a0cd92cb24d04be66557d63
MD.REDOWAN HOSSAIN <158824924+MD-REDOWAN@users.noreply.github.com>
2025-07-22 12:39:13 +0600

    The math.py has some built-in function and method like min(). max(), abs(), pow(), sqrt(), ceil(), floor() also math.pi

---
+++ Math.py
@@ -0,0 +1,68 @@
+
+
+# Python has a set of nuilt-in math funtions, including an extensive math module, that allows you to perform mathematical
+
+# Builit-in Math Functions min() and Max()
+
+x = min(10, 20, 30)
+y = max(10, 20, 30)
+
+print(x)
+print(y)
+
+# Output: 
+# 10
+# 30
+
+
+# The abs() function returns the absolute (positive) value of the specified number:
+
+a = abs(-5.12)
+
+print(a)
+
+# Output: 5.12
+
+
+
+# The pow(x, y) function return the value of x to the power of y:
+
+x = pow(5, 2)
+print(x)
+
+# Output: 25
+
+
+ 
+# The Math Module using sqrt() method for root
+
+import math
+x = math.sqrt(25)
+
+print(x)
+
+# Output: 5
+
+
+
+# The math.ceil() method rounds a number upwards to its nearest integer, and the math.floor() method rounds a number downwards to its nearest integer, and returns the result:
+
+import math
+
+x = math.ceil(2.5) # 3
+y = math.floor(2.5) #2
+
+print(x)
+print(y)
+
+
+
+# The math.pi constant, returns the value of PI (3.14...):
+
+import math
+x = math.pi
+
+print(x)
+
+# Output: 3.1415926
+
