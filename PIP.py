91a616b668ff73ef190f566b09ebf465d7a285bd
MD.REDOWAN HOSSAIN <158824924+MD-REDOWAN@users.noreply.github.com>
2025-07-23 11:14:58 +0600

    Install PIP package, using packages, Find package and list packages

---
+++ PIP.py
@@ -0,0 +1,23 @@
+
+
+# PIP is a package manager for Python packages, or modules if you like.
+
+import Function_part1
+
+c = Function_part1.redowan()
+
+txt = "hello world"
+
+print(c.hump(txt))
+
+
+# Install PIP Package
+
+# Download a Package
+
+# Using Packages
+
+# Find Package
+
+# List Packages
+
